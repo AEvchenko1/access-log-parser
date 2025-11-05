82c9130d06805fe52a33061119ed85925e8fe632
Andrey Evchenko <evcha90@mail.ru>
2025-11-05 21:23:54 +0300

    Add_elemental_actions_with_numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число:");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите первое число:");
+        int number2 = new Scanner(System.in).nextInt();
+
+        System.out.println("Сумма:");
+        int sum = number1+number2;
+        System.out.println(sum);
+
+        System.out.println("Разность:");
+        int sub = number1-number2;
+        System.out.println(sub);
+
+        System.out.println("Произведение:");
+        int multiply = number1*number2;
+        System.out.println(multiply);
+
+        System.out.println("Частное:");
+        double quo = (double) number1/number2;
+        System.out.println(quo);
     }
 }
